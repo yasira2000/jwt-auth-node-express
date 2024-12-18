736966daf1e3517dcd80536b86813a72ab8ce9fe
yasira2000 <yasirapunsith@gmail.com>
2024-12-18 23:13:23 +0530

    new access token creation endpoint created

--- api/server.js
+++ api/server.js
@@ -26,22 +26,87 @@ const users = [
   },
 ];
 
+// This array is only for demostration purpose.
+// In real world you have to store these refresh tokens in a database
+let refreshTokens = [];
+
+const generateAccessToken = (user) => {
+  return jwt.sign(
+    { id: user.id, isAdmin: user.isAdmin },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "20s" }
+  );
+};
+const generateRefreshToken = (user) => {
+  return jwt.sign(
+    { id: user.id, isAdmin: user.isAdmin },
+    process.env.REFRESH_TOKEN_SECRET
+  );
+};
+
+app.post("/api/refresh", (req, res) => {
+  // take the refresh token from the user
+  const refreshToken = req.body.refreshToken;
+
+  // Send error if there is no token or the token is invalid
+  if (!refreshToken)
+    return res.status(401).json({
+      success: false,
+      message: "You are not authenticated!",
+    });
+
+  if (!refreshTokens.includes(refreshToken)) {
+    return res.status(403).json({
+      success: false,
+      message: "Refresh token is not valid",
+    });
+  }
+
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      res.status(403).json({
+        success: false,
+        message: "Refresh token is not valid",
+      });
+    }
+
+    // Remove the current refresh token from the list and keep others refresh tokens
+    refreshTokens = refreshTokens.filter((token) => {
+      token !== refreshToken;
+    });
+
+    const newAccessToken = generateAccessToken(user);
+    const newRefreshToken = generateRefreshToken(user);
+
+    refreshTokens.push(newRefreshToken);
+
+    res.status(200).json({
+      success: true,
+      message: "New access token created.",
+      data: {
+        accessToken: newAccessToken,
+        refreshToken: newRefreshToken,
+      },
+    });
+  });
+
+  // If everything ok then create new access token and refresh token and send to the user
+});
+
 app.post("/api/login", (req, res) => {
   const { username, password } = req.body;
 
-  console.log(username, password);
   // check if the username and password match
   const user = users.find((u) => {
     return u.username === username && u.password === password;
   });
-  console.log(user);
   if (user) {
     // if username and password are matched then create a access token
-    const accessToken = jwt.sign(
-      { id: user.id, isAdmin: user.isAdmin },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "20s" }
-    );
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
+
+    refreshTokens.push(refreshToken);
+
     res.status(200).json({
       success: true,
       message: "Login successful",
@@ -49,6 +114,7 @@ app.post("/api/login", (req, res) => {
         username: user.username,
         isAdmin: user.isAdmin,
         accessToken,
+        refreshToken,
       },
     });
   } else {
@@ -59,7 +125,7 @@ app.post("/api/login", (req, res) => {
   }
 });
 
-// This is the middleware that we are going to use to identify
+// This is the middleware that we are going to use to identify,
 // if the user is authenticated before every user activity
 const verify = (req, res, next) => {
   const authHeader = req.headers.authorization;
